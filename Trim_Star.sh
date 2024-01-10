6e1cb2d68ff1dc4a80c86f2972cee1303b249a2e
katyrulezzz <116888495+katyrulezzz@users.noreply.github.com>
2024-01-10 12:54:27 +0300

    Add files via upload

---
+++ Trim_Star.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=TrimStar
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=10G
+#SBATCH --time=10:00:00
+#SBATCH --partition=basic
+#SBATCH --output=/scratch/course/2023w300106/katya/ex6/star.log
+#SBATCH --error=/scratch/course/2023w300106/katya/ex6/star.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=a12307340@unet.univie.ac.at
+
+### ENVIRONMENT
+module load star
+module list
+
+### SETTING UP DIRECTORIES
+# cd into my own directory
+cd /scratch/course/2023w300106/katya
+mkdir ex6
+cd ex6
+
+### RUNNING THE COMMAND
+STAR --runThreadN 16 \
+--genomeDir /scratch/course/2023w300106/katya/ncbi_dataset/data/GCF_932526225.1/jaNemVect1.1_STAR \
+--readFilesIn /scratch/course/2023w300106/katya/SRR24348401/trim/SRR24348401_R1.qc.fastq /scratch/course/2023w300106/katya/SRR24348401/trim/SRR24348401_R2.qc.fastq \
+--outSAMtype BAM SortedByCoordinate \
+--twopassMode Basic \
+--outSAMstrandField intronMotif \
+--outFileNamePrefix SRR24348401
